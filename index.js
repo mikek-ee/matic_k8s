02a2db339e2d8e4d03401fe145062eda44da4619
Mike <me@mikek.ee>
2022-02-13 18:10:50 +0000

    add initial heimdall node

--- index.js
+++ index.js
@@ -13,4 +13,10 @@ if (config["k8s"]) {
         const bornode = bor.createWorkload("bor", clusterProvider)
         exports.bor = bornode
     }
+
+    if (config["k8s-heimdall"]) {
+        const heimdall = require("./k8s/workloads/heimdall")
+        const heimdallNode = heimdall.createWorkload("heimdall", clusterProvider)
+        exports.heimdall = heimdallNode
+    }
 }
